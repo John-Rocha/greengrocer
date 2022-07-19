5795ea97ae9ec1e06f469916667b6da851b7d84c
Johnathan Rocha <johnathanrocha@gmail.com>
2022-07-19 01:17:14 -0300

    feat: created model of products

---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,15 @@
+class ItemModel {
+  final String description;
+  final String imgUrl;
+  final String itemName;
+  final double price;
+  final String unit;
+
+  ItemModel({
+    required this.description,
+    required this.imgUrl,
+    required this.itemName,
+    required this.price,
+    required this.unit,
+  });
+}
