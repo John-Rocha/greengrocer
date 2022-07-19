1612ea8c9ff6fd573a33ac7d13ccf74f86ff8f1b
Johnathan Rocha <johnathanrocha@gmail.com>
2022-07-19 14:17:24 -0300

    feat: created item tile

---
+++ lib/src/pages/home/components/item_tile.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+
+import 'package:greengrocer/src/models/item_model.dart';
+
+class ItemTile extends StatelessWidget {
+  const ItemTile({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final ItemModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shadowColor: Colors.grey.shade300,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Imagem
+            Expanded(
+              child: Image.asset(item.imgUrl),
+            ),
+
+            // Nome
+            Text(
+              item.itemName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            // Preço - Unidade
+            Row(
+              children: [
+                Text(
+                  'R\$ ${item.price.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: CustomColors.customSwatchColor,
+                  ),
+                ),
+                Text(
+                  '/${item.unit}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                    color: Colors.grey.shade500,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
