377578075e369a65cf1913b4ffc91dba74b36b2b
Johnathan Rocha <johnathanrocha@gmail.com>
2022-07-21 19:42:52 -0300

    feat: adds vertical spacing and correct status

--- lib/src/pages/orders/components/order_status_widget.dart
+++ lib/src/pages/orders/components/order_status_widget.dart
@@ -3,41 +3,82 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class OrderStatusWidget extends StatelessWidget {
-  const OrderStatusWidget({
+  final String status;
+  final bool isOverdue;
+
+  final Map<String, int> allStatus = {
+    'pending_payment': 0,
+    'refunded': 1,
+    'paid': 2,
+    'preparing_purchase': 3,
+    'shipping': 4,
+    'deliverd': 5
+  };
+
+  int get currentStatus => allStatus[status]!;
+
+  OrderStatusWidget({
     Key? key,
     required this.status,
     required this.isOverdue,
   }) : super(key: key);
 
-  final String status;
-  final bool isOverdue;
-
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _StatusDot(
-          title: 'Teste de pagamento',
           isActive: true,
+          title: 'Pedido confirmado',
         ),
-        _StatusDot(
-          title: 'Pagamento efetuado',
-          isActive: false,
-        ),
+        const _CustomDivider(),
+        if (currentStatus == 1) ...[
+          const _StatusDot(
+            isActive: true,
+            title: 'Pix estornado',
+            backgroundColor: Colors.orange,
+          )
+        ] else if (isOverdue) ...[
+          const _StatusDot(
+            isActive: true,
+            title: 'Pagamento pix vencido',
+            backgroundColor: Colors.red,
+          ),
+        ]
       ],
     );
   }
 }
 
+class _CustomDivider extends StatelessWidget {
+  const _CustomDivider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 10,
+      width: 2,
+      color: Colors.grey.shade300,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 3,
+      ),
+    );
+  }
+}
+
 class _StatusDot extends StatelessWidget {
+  final bool isActive;
+  final String title;
+  final Color? backgroundColor;
+
   const _StatusDot({
     Key? key,
     required this.isActive,
     required this.title,
+    this.backgroundColor,
   }) : super(key: key);
-
-  final bool isActive;
-  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -53,8 +94,9 @@ class _StatusDot extends StatelessWidget {
             border: Border.all(
               color: CustomColors.customSwatchColor,
             ),
-            color:
-                isActive ? CustomColors.customSwatchColor : Colors.transparent,
+            color: isActive
+                ? backgroundColor ?? CustomColors.customSwatchColor
+                : Colors.transparent,
           ),
           child: isActive
               ? const Icon(
@@ -66,7 +108,12 @@ class _StatusDot extends StatelessWidget {
         ),
         const SizedBox(width: 5),
         Expanded(
-          child: Text(title),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 12,
+            ),
+          ),
         ),
       ],
     );
